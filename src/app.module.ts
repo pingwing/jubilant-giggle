23acad40303d8fd64b569df997b6bb02c68fe0a1
Przemysław Gierski <pgierski@o2.pl>
2025-04-17 17:35:41 +0200

    create data models / GraphQL schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
+import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+
+const localEnvironment = process.env.ENVIRONMENT === 'LOCAL';
+
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      graphiql: localEnvironment ? true : false,
+      typePaths: ['./**/*.graphql'],
+      definitions: {
+        path: join(process.cwd(), 'src/graphql.ts'),
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
